6ee53b6f151e870f9e5625be93f8559683f333b4
k-yomo <kanji.yy@gmail.com>
2019-10-06 11:36:31 +0900

    Implement helper functions

--- firestore_tx_manager.go
+++ firestore_tx_manager.go
@@ -38,3 +38,48 @@ func GetTx(ctx context.Context) (*firestore.Transaction, bool) {
 	return tx, ok
 }
 
+// CreateWithTx inserts data into document with transaction if exist. If not, it will create as usual.
+func CreateWithTx(ctx context.Context, dr *firestore.DocumentRef, data interface{}) error {
+	tx, ok := GetTx(ctx)
+	if ok {
+		return tx.Create(dr, data)
+	} else {
+		_, err := dr.Create(ctx, data)
+		return err
+	}
+}
+
+// SetWithTx set data to document with transaction if exist. If not, it will set as usual.
+func SetWithTx(ctx context.Context, dr *firestore.DocumentRef, data interface{}, opts ...firestore.SetOption) error {
+	tx, ok := GetTx(ctx)
+	if ok {
+		return tx.Set(dr, data, opts...)
+	} else {
+		_, err := dr.Set(ctx, data, opts...)
+		return err
+	}
+}
+
+
+// UpdateWithTx updates document with transaction if exist. If not, it will update as usual.
+func UpdateWithTx(ctx context.Context, dr *firestore.DocumentRef, data []firestore.Update, preconds ...firestore.Precondition) error {
+	tx, ok := GetTx(ctx)
+	if ok {
+		return tx.Update(dr, data, preconds...)
+	} else {
+		_, err := dr.Update(ctx, data, preconds...)
+		return err
+	}
+}
+
+
+// DeleteWithTx deletes document with transaction if exist. If not, it will delete as usual.
+func DeleteWithTx(ctx context.Context, dr *firestore.DocumentRef, preconds ...firestore.Precondition) error {
+	tx, ok := GetTx(ctx)
+	if ok {
+		return tx.Delete(dr, preconds...)
+	} else {
+		_, err := dr.Delete(ctx, preconds...)
+		return err
+	}
+}
